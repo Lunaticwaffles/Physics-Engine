10dba96374bb577af7bfccb077850f79fe123735
Lunaticwaffles <69997451+Lunaticwaffles@users.noreply.github.com>
2020-09-04 20:57:00 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,16 @@
+class box {
+    constructor(){
+        var options = {
+            restitution: 1
+        }
+        this.body = Bodies.rectangle(200,100,50,50);
+        World.add(world,this.body);
+        display {
+            var pos = this.body.position;
+            rectMode(CENTER);
+            fill(255);
+            rect(pos.x,pos.y,this.width,this.height);
+        }
+
+    }
+}
